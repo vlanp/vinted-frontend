17164977ce524bd29fe911c72076b339345de803
Valentin <guillaumevalentin76@gmail.com>
2024-06-12 13:07:33 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes></Routes>
+    </Router>
+  );
+}
+
+export default App;
